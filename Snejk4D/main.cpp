5b7daed4fe130ab08e948366f1263f98a1bb79db
DESKTOP-14FF54E\Senpai <tamburyniarz@gmail.com>
2016-08-18 19:18:11 +0200

    Init GLFW widnow

--- Snejk4D/main.cpp
+++ Snejk4D/main.cpp
@@ -1,8 +1,35 @@
 #include <cstdio>
 #include <iostream>
 
+#include <glfw3.h>
+GLFWwindow* window;
+
 int main()
 {
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Snejk 4D", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
 	int a = 1;
 
 	std::cout << "Hello git. #" << a;
