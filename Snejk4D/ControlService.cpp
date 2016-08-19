f84b69ad7364def9150224feb8c4da972be10863
DESKTOP-14FF54E\Senpai <tamburyniarz@gmail.com>
2016-08-19 20:24:44 +0200

    add controlService class

---
+++ Snejk4D/ControlService.cpp
@@ -0,0 +1,22 @@
+﻿#include "ControlService.h"
+
+void ControlService::moveMatrixFromInputs()
+{
+
+}
+
+
+
+/*
+	Getters and setters
+*/
+
+glm::mat4 ControlService::getViewMatrix()
+{
+	return ViewMatrix;
+}
+
+glm::mat4 ControlService::getMoveMatrix()
+{
+	return MovewMatrix;
+}
---
+++ Snejk4D/ControlService.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "gl_includes.h"
+
+class ControlService
+{
+public:
+	void moveMatrixFromInputs();
+
+	glm::mat4 getViewMatrix();
+	glm::mat4 getMoveMatrix();
+
+	glm::mat4 MovewMatrix;
+	glm::mat4 ProjectionMatrix;
+	glm::mat4 ViewMatrix;
+};
