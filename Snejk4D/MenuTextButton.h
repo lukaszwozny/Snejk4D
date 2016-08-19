8e304b79e5f38f281647a0a906c6e0a7d3903856
DESKTOP-14FF54E\Senpai <tamburyniarz@gmail.com>
2016-08-19 02:11:59 +0200

    Added fileService.class and MenuTextButton.class

---
+++ Snejk4D/MenuTextButton.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <vector>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLM
+#include <glm/gtc/matrix_transform.hpp>
+
+class MenuTextButton
+{
+public:
+	MenuTextButton(char* path);
+	void Display(glm::vec3 pos);
+private:
+	GLuint programID;
+	GLuint Texture;
+	GLuint TextureID;
+	GLuint MatrixID;
+
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> uvs;
+	std::vector<glm::vec3> normals; // Won't be used at the moment.
+
+	GLuint vertexbuffer;
+	GLuint uvbuffer;
+	glm::mat4 ModelMatrix;
+	glm::mat4 MVP;
+};
